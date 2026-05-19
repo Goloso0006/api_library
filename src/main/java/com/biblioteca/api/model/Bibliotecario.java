8badce9eb0fe8638b44a194fbccdd7cbfff86eab
Marlon Alexander I. <marlon1906ia@gmail.com>
2026-05-18 22:14:17 -0500

    Delete import Document of layer models

--- src/main/java/com/biblioteca/api/model/Bibliotecario.java
+++ src/main/java/com/biblioteca/api/model/Bibliotecario.java
@@ -3,7 +3,6 @@ package com.biblioteca.api.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
